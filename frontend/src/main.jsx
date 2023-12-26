f4655514924c00a34d978ecc4872de1c584d412f
Ilya Sinyakin <arqu1t@mail.ru>
2023-12-26 16:01:17 +0300

    v1

---
+++ frontend/src/main.jsx
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import UserStore from "./store/UserStore";
+import DeviceStore from "./store/DeviceStore";
+
+export const Context = createContext(null);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Context.Provider
+    value={{
+      user: new UserStore(),
+      device: new DeviceStore(),
+    }}
+  >
+    <App />
+  </Context.Provider>
+);
